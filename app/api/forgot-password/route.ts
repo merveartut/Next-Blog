e5e5dcbd8859dde7916d598e8715891bc16d0945
merveartut <merveartuttt@gmail.com>
2026-02-27 15:41:44 +0300

    reset password feature updated

--- app/api/forgot-password/route.ts
+++ app/api/forgot-password/route.ts
@@ -5,7 +5,6 @@ import { v4 as uuidv4 } from "uuid";
 import { Resend } from "resend";
 
 const prisma = new PrismaClient();
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   const apiKey = process.env.RESEND_API_KEY;
