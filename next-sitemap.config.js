1d4d2d86f93c888d7ee56c6f9c3687f5413bc1ec
merveartut <merveartuttt@gmail.com>
2026-02-27 14:43:44 +0300

    sitemap updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   siteUrl: "https://www.thebelog.com",
   generateRobotsTxt: true,
-  exclude: ["/admin/*"], // Gizli sayfaları çıkar
+  sitemapSize: 7000,
+  // 💡 ÖNEMLİ: Eğer yazıların dinamikse ve build anında çekilmesini istiyorsan
+  // transform fonksiyonuyla sayfalarını kontrol etmelisin.
 };
