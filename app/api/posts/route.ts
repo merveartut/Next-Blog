08b5b21bb9d30cec81c5b8ae31252ee8e6991280
merveartut <merveartuttt@gmail.com>
2026-03-02 16:49:41 +0300

    file size limit deleted

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -3,14 +3,6 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: "10mb", // Limiti 10MB'a çıkardık
-    },
-  },
-};
-
 export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
